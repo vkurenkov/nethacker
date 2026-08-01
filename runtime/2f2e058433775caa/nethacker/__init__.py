52266b0ea2917fcef3b53069f55f3f6b25aa56d8
Vladislav Kurenkov <castielblak@gmail.com>
2026-08-01 18:09:13 +0300

    Store NetHacker source 8afc8f4e557e

---
+++ runtime/2f2e058433775caa/nethacker/__init__.py
@@ -0,0 +1,6 @@
+"""NetHacker distributed symbolic-agent evolution infrastructure."""
+
+from nethacker.constants import BASELINE_COMMIT, BASELINE_REPOSITORY
+
+__all__ = ["BASELINE_COMMIT", "BASELINE_REPOSITORY"]
+__version__ = "0.1.0"
