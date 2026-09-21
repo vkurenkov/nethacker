1d21cb0da47249abce6243f1d7b87b8d406a905d
Vladislav Kurenkov <castielblak@gmail.com>
2026-09-21 14:43:14 +0300

    nethackers evolve win

--- autoascend/item/item_manager.py
+++ autoascend/item/item_manager.py
@@ -257,11 +257,13 @@ class ItemManager:
 
         assert category not in [nh.RANDOM_CLASS]
 
+        # hypothesis: recognize the moist towel state as an item condition,
+        # preventing a normal moisture change from terminating the run.
         matches = re.findall(
             r'^(a|an|the|\d+)'
             r'( empty)?'
             r'( (cursed|uncursed|blessed))?'
-            r'( (very |thoroughly )?(rustproof|poisoned|corroded|rusty|burnt|rotted|partly eaten|partly used|diluted|unlocked|locked|wet|greased))*'
+            r'( (very |thoroughly )?(rustproof|poisoned|corroded|rusty|burnt|rotted|partly eaten|partly used|diluted|unlocked|locked|wet|moist|greased))*'
             r'( ([+-]\d+))? '
             r"([a-zA-z0-9-!'# ]+)"
             r'( \(([0-9]+:[0-9]+|no charge)\))?'
